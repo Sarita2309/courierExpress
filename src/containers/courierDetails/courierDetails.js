34179042c71203f9e6ecdd04a28e36c79c4bb904
Shivani <shivani.singh@greatwits.com>
2019-10-04 14:48:27 +0530

    folder naming

---
+++ src/containers/courierDetails/courierDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CourierDetails extends Component {
+    render() {
+        return (
+            <div>
+                <p> courierDetails will display here....</p>
+            </div>
+        )
+    }
+}
+
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-
+import { BrowserRouter, Switch ,Route} from 'react-router-dom';
+import CourierDetails from './containers/courierDetails/courierDetails'
 
 class Routing extends Component {
- 
   render() {
     return (
       <BrowserRouter>
         <div>
           <Switch>
-            
+            <Route path='/courierDetails' component={CourierDetails}/>
           </Switch>
         </div>
-
       </BrowserRouter>
     );
   }
