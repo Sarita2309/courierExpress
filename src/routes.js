d34d50187a46dbf378d42b1b98e8be49141d5979
Abhinav Kumar <abhinav.kumar@greatwits.com>
2019-10-04 12:26:44 +0530

    f-c

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+
+class Routing extends Component {
+ 
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            
+          </Switch>
+        </div>
+
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Routing;
